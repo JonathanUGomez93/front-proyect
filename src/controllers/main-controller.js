dfb65288fde526363417c92649ac6fdcb7d1e2b1
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-03-01 18:05:39 -0300

    add logic to controllers and views to use db. Basic

--- src/controllers/main-controller.js
+++ src/controllers/main-controller.js
@@ -3,21 +3,39 @@ const db = require('../database/models/index');
 const controller = {
     index: async (req, res) => {
         try {
-            const cards = await db.Card.findAll();
+            let cards = await db.Card.findAll();
             res.render("index", { cards: cards });
-        } catch (err) { 
+        } catch (err) {
             console.log(err);
             res.status(500).send('La cagaste weon')
         }
     },
-    new: (req, res) => {
-        res.render("new")
+    new: async (req, res) => {
+        try {
+            let cards = await db.Card.findAll();
+            res.render("new", { cards: cards });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('La cagaste weon')
+        }
     },
-    featured: (req, res) => {
-        res.render("featured")
+    featured: async (req, res) => {
+        try {
+            let cards = await db.Card.findAll();
+            res.render("featured", { cards: cards });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('La cagaste weon')
+        }
     },
-    sale: (req, res) => {
-        res.render("sale")
+    sale: async (req, res) => {
+        try {
+            let cards = await db.Card.findAll();
+            res.render("sale", { cards: cards });
+        } catch (err) {
+            console.log(err);
+            res.status(500).send('La cagaste weon')
+        }
     },
     contact: (req, res) => {
         res.render("contact")
@@ -25,5 +43,5 @@ const controller = {
     sealed: (req, res) => {
         res.render("sealed")
     }
-}
+};
 module.exports = controller
