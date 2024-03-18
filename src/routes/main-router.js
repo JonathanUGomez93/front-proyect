30029f2ab5e7823c3047d79002d04aaaa04af298
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-03-18 19:17:46 -0300

    starting the card detail on mobile resolution. Basic structure

--- src/routes/main-router.js
+++ src/routes/main-router.js
@@ -12,6 +12,7 @@ router.get("/featured", mainController.featured)
 router.get("/sale", mainController.sale)
 router.get("/contactUs", mainController.contact)
 router.get("/sealed", mainController.sealed)
+router.get("/productdetail/:id", mainController.detail)
 
 
 module.exports = router;
