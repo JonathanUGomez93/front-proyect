5adc14ccda527efd59fe5649ce535be3130b6030
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-06-05 17:44:06 -0300

    start work on search input

---
+++ public/js/searchBar.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', async function () { 
+    const searchInput = document.querySelector('.inputNav')
+    const searchButton = document.querySelector('.searchButtonNav')
+
+})
