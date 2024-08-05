7267f24f14d62444783be6654c572d37e5583f7a
JonathanUGomez93 <jonathanurielgomez1993@gmail.com>
2024-08-05 19:15:17 -0300

    starting commander content

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -3,17 +3,30 @@ document.addEventListener('DOMContentLoaded', async function () {
     const dropdownSingles = document.querySelector('.singles')
     //menu de sellado
     const dropdownSealed = document.querySelector('.sealed')
+    //menu de cuenta
+    const dropdownAccount = document.querySelector('.account')
+
+
     //div de las opciones de menu de sellado
     const sealedOptions = document.querySelector('.sealedOptions')
     //div de opciones con clase visibility. Default es hidden
     const dropdownVisibility = document.querySelector('.dropdownVisibility')
+    //div opciones de cuenta
+    const accountOptions = document.querySelector('.accountOptions')
 
     dropdownSingles.addEventListener('click', () => {
         dropdownVisibility.classList.toggle('dropdownVisibility');
         sealedOptions.classList.add('dropdownVisibility');
+        accountOptions.classList.add('dropdownVisibility');
     })
     dropdownSealed.addEventListener('click', () => {
         sealedOptions.classList.toggle('dropdownVisibility');
         dropdownVisibility.classList.add('dropdownVisibility');
+        accountOptions.classList.add('dropdownVisibility');
+    })
+    dropdownAccount.addEventListener('click', () => {
+        accountOptions.classList.toggle('dropdownVisibility');
+        dropdownVisibility.classList.add('dropdownVisibility');
+        sealedOptions.classList.add('dropdownVisibility');
     })
 })
